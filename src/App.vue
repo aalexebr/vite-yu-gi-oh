ec4214f715e076b24f070b136f1122487ce929eb
aalexebr <130670504+aalexebr@users.noreply.github.com>
2023-07-17 15:25:15 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import componentTemplate from './components/componentTemplate.vue'
+
+export default{
+    data(){
+      return{}
+    },
+    components: {
+      componentTemplate
+    },
+}
+</script>
+
+<template>
+  <componentTemplate/>
+</template>
+
+<style lang="scss">
+@use './assets/scss/main.scss' as *;
+</style>
---
+++ src/components/componentTemplate.vue
@@ -0,0 +1,20 @@
+<script>
+export default{
+    data(){
+      return{}
+    },
+    components: {
+
+    }
+}
+</script>
+
+<template>
+<div>
+  templae
+</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
